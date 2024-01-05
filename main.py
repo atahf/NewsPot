0dd61fc0db9933924f377349d4fe849a2779498e
atahf <ataollah@sabanciuniv.edu>
2024-01-05 20:19:53 +0300

    last configuration

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from website import creat_app
 app = creat_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(debug=False, host="0.0.0.0", port="80")
+    app.run(debug=False, host="0.0.0.0", port="8080")
